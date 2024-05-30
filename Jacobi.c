6302704d728fe50270e7a07b3f79f6bd7c4d0c3c
MatheusMenezesss <159057394+MatheusMenezesss@users.noreply.github.com>
2024-05-30 11:56:28 -0300

    Update Jacobi.c

--- Jacobi.c
+++ Jacobi.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 long int variable_number, interactions;
 double **Matrix;
 
+void Matrix_create();
+void Matrix_destruct();
+
 int main(){
     printf("escreva o numero de variáveis que estamos trabalhando: ");scanf("%ld", &variable_number);
-    printf("\nescreva o numero de interacoes a serem feitas: ");scanf("%ld", &interactions);
+    printf("escreva o numero de interacoes a serem feitas: ");scanf("%ld", &interactions);
     printf("Descreva a Matrix:\nEX Matrix de 3 variaveis:\n2X1+6X2-1X3=5\n3X1+0X2+8X3=2\n1X1+3X2-5X3=0\n");
 
     return 0;
 }
 
-void get_Matrix(){
-    for(int i = 0; i<variable_number; i++){
-        
+void add_char(char *string, char caracter){
+    if(string){
+    }else{
+
     }
+}
+
+char* get_string(){
+    char carac, *string;
+    string = NULL;
+
+    do{
+        scanf("%c", &carac);
+        if(carac != '\n'){
+
+        }
+    }while (carac != '\n');
+    
+}
+
+void get_Matrix(){
+    char *aux = (char*) malloc(sizeof(char)*50); //vamos assumir que o numero de caracteres das equações não passe de 49
+    int count_caracter;
+    for(int i = 0; i<variable_number; i++){
+    }
+
+}
+
+void Matrix_create(){
+    Matrix = (double*) malloc(sizeof(double*) * variable_number);
+    for(int i = 0; i < variable_number; i++){
+        Matrix[i] = (double*) malloc(sizeof(double) * variable_number);
+    }
+}
+
+void Matrix_destruct(){
 
 }
